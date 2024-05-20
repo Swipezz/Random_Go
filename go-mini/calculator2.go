f741452d4389bf777e8f492b30016eeda1191095
Swipezz <85165739+Swipezz@users.noreply.github.com>
2024-05-20 17:39:39 +0000

    Simpen dulu

--- go-mini/calculator2.go
+++ go-mini/calculator2.go
@@ -1,3 +1,32 @@
-func Calculator2(nomer int) {
-	
+package gomini
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func Calculator2() {
+	var stringNumber string
+	var stringFunc string
+	var intNumber []int
+
+	var kunci int
+
+	fmt.Println("Format calculator | 1+2 |")
+	fmt.Scanln(&stringNumber)
+	for i, char := range stringNumber {
+		if !unicode.IsDigit(char) {
+			if string(char) != "+" || string(char) != "-"  {
+				
+			}
+			stringFunc = string(char)
+		} else {
+			if kunci == 0 {
+				intNumber[0] += int(char) 
+			}
+			if kunci == 1 {
+				intNumber[1] += int(char) 
+			}
+		} 		
+	}
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	gomini.Roulette()
+	gomini.Calculator2()
 }
